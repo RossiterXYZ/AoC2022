27937f512517c68537403dbf2c70de239642f8b5
Michael Rossiter <michael@rossiter.xyz>
2022-12-02 05:09:06 +0800

    first commit

---
+++ Day1/Puzzle1.py
@@ -0,0 +1,14 @@
+CurrentElfCalories = 0
+ElfData = []
+
+with open("Day1\Input.txt") as InputFile: # Use file to refer to the file object
+   for InputLine in InputFile:
+        if InputLine == "\n":
+            ElfData.append(CurrentElfCalories)
+            CurrentElfCalories = 0
+        else:
+            CurrentElfCalories += int(InputLine)
+ElfData.sort(reverse=True)
+
+print(f'Puzzle 1: Highest Elf = {ElfData[0]}')
+print(f'Puzzle 1: Highest 3 Elves = {ElfData[0] + ElfData[1] + ElfData[2]}')
