6ffbf9d2b4f50b84277ce8f4b4cae3096849b869
Michael Rossiter <michael@rossiter.xyz>
2022-12-03 16:38:19 +0800

    Slight Code Refactor

--- Day3/Puzzle.py
+++ Day3/Puzzle.py
@@ -1,6 +1,14 @@
 #Puzzle 1
 Count1 = 0
 
+def ElvesValue(_Value):
+    Value = ord(_Value)
+    if(Value > 96):
+        Value -= 96
+    else:
+        Value -= 38
+    return Value
+
 with open("Day3\Input.txt") as InputFile:
     for InputLine in InputFile:
         InputLine = InputLine.strip()
@@ -8,13 +16,8 @@ with open("Day3\Input.txt") as InputFile:
         FirstHalf = set(InputLine[:HalfStringLength])
         SecondHalf = set(InputLine[HalfStringLength:])
 
-        Answer = ord(FirstHalf.intersection(SecondHalf).pop())
-        if(Answer > 96):
-            Answer -= 96
-        else:
-            Answer -= 38
-
-        Count1 += Answer
+        Answer = FirstHalf.intersection(SecondHalf).pop()
+        Count1 += ElvesValue(Answer)
 
 Iterator = 0
 Count2 = 0
@@ -29,13 +32,8 @@ with open("Day3\Input.txt") as InputFile:
         else:
             Iterator = 0
 
-        Answer = ord(ThreeSacks[0].intersection(ThreeSacks[1]).intersection(ThreeSacks[2]).pop())
-
-        if(Answer > 96):
-            Answer -= 96
-        else:
-            Answer -= 38
-        Count2 += Answer
+        Answer = ThreeSacks[0].intersection(ThreeSacks[1]).intersection(ThreeSacks[2]).pop()
+        Count2 += ElvesValue(Answer)
 
 print(f'Puzzle 1: Common Item = {Count1}')
 print(f'Puzzle 2: Found Badges = {Count2}')
